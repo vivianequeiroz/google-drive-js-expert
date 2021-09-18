ab23c0f6a13e4b4964eede6095c545199b23052a
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-09-18 18:21:30 -0300

    feat: upload progress

--- aulas/aula03/google-drive-webapp/public/src/controller/appController.js
+++ aulas/aula03/google-drive-webapp/public/src/controller/appController.js
@@ -9,24 +9,54 @@ export default class AppController {
     this.viewManager.configureFileBtnClick();
     this.viewManager.configureModal();
     this.viewManager.configureOnFileChange(this.onFileChange.bind(this));
-    this.connectionManagerService.configureEvents(() => {});
-
-    this.viewManager.openModal();
-    setTimeout(() => {
-      this.viewManager.closeModal();
-    }, 2000);
+    this.connectionManagerService.configureEvents({
+      onProggres: this.onProgress.bind(this),
+    });
+    this.viewManager.updateStatus(0);
 
     await this.updateCurrentFiles();
   }
 
+  async onProgress({ processedAlready, filename }) {
+    console.debug({});
+    const file = this.uploadingFiles.get(filename);
+    const alreadyProcessed = Math.ceil((processedAlready / file.size) * 100);
+
+    updateProgress(file, alreadyProcessed);
+
+    if (alreadyProcessed < 98) return;
+
+    return this.updateCurrentFiles();
+  }
+
+  async updateProgress(file, percent) {
+    const uploadingFiles = this.uploadingFiles;
+    file.percent = percent;
+
+    const total = [...uploadingFiles.values()]
+      .map(({ percent }) => percent ?? 0)
+      .reduce((total, current) => total + current, 0);
+
+    this.viewManager.updateStatus(total);
+  }
+
   async onFileChange(files) {
     const requests = [];
+
+    this.viewManager.openModal();
+    this.viewManager.updateStatus(0);
+
     for (const file of files) {
       this.uploadingFiles.set(file.name, file);
       requests.push(this.connectionManagerService.uploadFile(file));
     }
 
     await Promise.all(requests);
+    this.viewManager.updateStatus(100);
+
+    setTimeout(() => {
+      this.viewManager.closeModal();
+    }, 1000);
 
     await this.updateCurrentFiles();
   }
